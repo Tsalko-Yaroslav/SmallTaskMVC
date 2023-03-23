27b019b3bb391290bc7b5882d396a4aae7d9b7da
Yaroslav Tsalko <ipz212_tsyav@student.ztu.edu.ua>
2023-03-23 16:06:09 +0200

    Add project files.

---
+++ SmallTaskMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SmallTaskMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
